b755e27511dc046603311f7d282e9e7b6feb01a1
shinta <flekystyley@gmail.com>
2024-02-09 15:49:23 +0900

    initial commits

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "xv6z";
+
+  inputs = {
+    nixpkgs = { url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
+    flake-utils = { url = "github:numtide/flake-utils"; };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            zig
+            zls
+            qemu
+          ];
+        };
+      }
+    );
+}
